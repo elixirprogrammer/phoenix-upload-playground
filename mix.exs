82de6f73f861cb30203e0d288ea212686fd35061
Anthony Gonzalez <boilercoding@gmail.com>
2020-11-11 12:57:37 -0500

    adds waffle deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,13 @@ defmodule UploadPlayground.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:waffle, "~> 1.1.3"},
+      # If using S3:
+      {:ex_aws, "~> 2.1.2"},
+      {:ex_aws_s3, "~> 2.0"},
+      {:hackney, "~> 1.9"},
+      {:sweet_xml, "~> 0.6"}
     ]
   end
 
